fe3d414804b3fdef083794868a8eba7c738d6b83
Matthew <Matt@Matthews-MacBook-Pro.local>
2020-07-20 12:24:01 -0500

    Created test code file.

--- player_spec.rb
+++ player_spec.rb
@@ -1,10 +1,40 @@
 require_relative 'player'
+require_relative 'spec_helper'
+
+
+
 
 describe 'player' do
+    before do
+        @initial_health = 150
+        @player = Player.new("larry", @initial_health)
+    end
+    before do
+        $stdout = StringIO.new
+    end
     
     it "has a capitalized name" do
-        player = Player.new("larry", 150)
-        player.name.should == "Larry"
+        expect(@player.name).to eq("Larry")
     end
 
+    it "has an initial health" do
+        expect(@player.health).to eq(150)
+    end
+
+    it "has a string representation" do
+       expect(@player.to_s).to eq("I'm Larry with a health of 150 and a score of 155.")
+    end
+
+    it "computes a score as a sum of its health and length of name" do
+       expect(@player.score).to eq(150 + 5)
+    end
+        
+    it "increases health by 15 when w00ted" do
+        @player.w00t
+        expect(@player.health).to eq(@initial_health +15)
+    end
+    it "decreases health by 10 when blammed" do
+        @player.blam
+        expect(@player.health).to eq(@initial_health - 10)
+    end
 end
